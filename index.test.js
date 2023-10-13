1afd8f0f608658dc12668bfed315cde2cbc0bf0d
carlossdr03s <carlossodreibm@gmail.com>
2023-10-12 23:07:14 -0300

    fix: test cases

--- index.test.js
+++ index.test.js
@@ -4,15 +4,3 @@ const addLength = require('./index')
 test('gives two words', () => {
   expect(addLength('apple ban')).toEqual(['apple 5', 'ban 3'])
 })
-
-// test('passes an empty array', () => {
-//   expect(addLength('you will win')).toEqual(['you 3', 'will 4', 'win 3'])
-// })
-
-// test('passes a number', () => {
-//   expect(addLength(2)).toEqual(expect.any([String]))
-// })
-
-// test('passes a string without space', () => {
-//   expect(addLength('appleban')).toEqual(null)
-// })
